406ee8250353f64b974583661126abafa65cb8f8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-10-19 19:31:32 +0200

    Move data to separate file

---
+++ src/rest-utils.el
@@ -0,0 +1,50 @@
+(defun rest-utils-clear-current-line ()
+  (delete-region (line-beginning-position) (line-end-position)))
+
+(defun rest-utils--update-line (new-content)
+  (rest-utils-clear-current-line)
+  (insert new-content)
+  (if (= (point) (point-max))
+      (insert "\n")
+    (forward-line))
+  t)
+
+(defun rest-utils--update-current-line (operator)
+  (let* ((current-line (current-line))
+         (last-line-detected (is-last-line?)))
+    (let ((result (funcall operator current-line last-line-detected)))
+      (if result
+          (rest-utils--update-line result)        
+        nil))))
+
+(defun update-page (operator)
+  "Execute operator on all potential lines in the visible buffer
+
+The operator will be called for each line with the line content,
+and an t if the line is the last line (or nil otherwise) for as
+long as it takes to fill all lines potentially visible in the
+buffer.
+
+E.g. a buffer of 40 visible lines with only 3 text lines (the rest is empty):
+
+    foo
+    bar
+    baz
+
+Will be called 40 times, the first 2 with the lines contents and
+nil, one time with \"baz\" and t, and the remaining 37 ones with
+\"\" and t (unless the operator returns nil first)
+
+The return value of operator is the output that will replace the
+line, or nil to interrupt the recursion.
+
+The content is re-displayed at each insertion"
+  (save-excursion
+    (let ((lines (window-body-height))
+          (continue t)
+          (current-line 0))
+      (goto-char (window-start))
+      (while (and continue (< current-line lines))
+        (setq continue (rest-utils--update-current-line operator))
+        (redisplay)
+        (setq current-line (+ current-line 1))))))
--- src/utils.el
+++ src/utils.el
@@ -14,56 +14,13 @@
   (= (point-max)
      (line-end-position)))
 
-(defun clear-current-line ()
-  (delete-region (line-beginning-position) (line-end-position)))
 
-(defun update-line (new-content)
-  (clear-current-line)
-  (insert new-content)
-  (if (= (point) (point-max))
-      (insert "\n")
-    (forward-line))
-  t)
 
-(defun update-current-line (operator)
-  (let* ((current-line (current-line))
-         (last-line-detected (is-last-line?)))
-    (let ((result (funcall operator current-line last-line-detected)))
-      (if result
-          (update-line result)        
-        nil))))
 
-(defun update-page (operator)
-  "Execute operator on all potential lines in the visible buffer
 
-The operator will be called for each line with the line content,
-and an t if the line is the last line (or nil otherwise) for as
-long as it takes to fill all lines potentially visible in the
-buffer.
 
-E.g. a buffer of 40 visible lines with only 3 text lines (the rest is empty):
 
-    foo
-    bar
-    baz
 
-Will be called 40 times, the first 2 with the lines contents and
-nil, one time with \"baz\" and t, and the remaining 37 ones with
-\"\" and t (unless the operator returns nil first)
-
-The return value of operator is the output that will replace the
-line, or nil to interrupt the recursion.
-
-The content is re-displayed at each insertion"
-  (save-excursion
-    (let ((lines (window-body-height))
-          (continue t)
-          (current-line 0))
-      (goto-char (window-start))
-      (while (and continue (< current-line lines))
-        (setq continue (update-current-line operator))
-        (redisplay)
-        (setq current-line (+ current-line 1))))))
 
 (defun print-arguments (content last-line)
   (print "\n\n\n\n")
