222400b0f2cb9be5bc569946c9609d64e74743fb
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-10-24 08:49:14 +0200

    Posts now have a text property :)

--- src/rest-posts.el
+++ src/rest-posts.el
@@ -1,8 +1,13 @@
 (require 'rest-state)
 (require 'rest-utils)
 
+(defconst rest-posts--id-property 'post-id)
+
+(defun rest-posts--get-post-id (post)
+  (cdr (assoc 'id post)))
+
 (defun rest-posts--format-post (post get-user-f)
-  (let* ((id (cdr (assoc 'id post)))
+  (let* ((id (rest-posts--get-post-id post))
          (user-for-id (funcall get-user-f (cdr (assoc 'userId post))))
          (user-name (cdr (assoc 'name user-for-id))))
     (format "* id:%d <%s> %s"
@@ -14,6 +19,15 @@
   "Format the post representation using the caching version of the get-user function"
   (rest-posts--format-post post 'rest-state--get-user-with-id))
 
+(defun rest-posts--prepare-post-for-insertion (post)
+  "Format the post and add a text property with its ID"
+  (let ((formatted-post (rest-posts--format-post-with-user post))
+        (post-id (rest-posts--get-post-id post)))
+    (put-text-property 0 (length formatted-post)
+                       rest-posts--id-property post-id
+                       formatted-post)
+    formatted-post))
+
 (defun rest-posts--insert-posts ()
   "Insert *all* posts of the service in the page.
 
@@ -24,7 +38,7 @@ Pagination would be a nice idea, but the API dosen't support it"
                 (insert post-as-string)
                 (insert "\n")
                 (redisplay))
-              (mapcar 'rest-posts--format-post-with-user (rest-api--read-posts))))))
+              (mapcar 'rest-posts--prepare-post-for-insertion (rest-api--read-posts))))))
 
 (defun jump-to-post ()
   (interactive)
