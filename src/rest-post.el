36c5936c6c7bedfa5e573f3499601318cdb1c67c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-10-26 18:30:45 +0200

    I have a post introspection of sorts
    It looks terrible

--- src/rest-post.el
+++ src/rest-post.el
@@ -6,8 +6,18 @@
 (defun rest-post--bind-keys ()
   (local-set-key (kbd "q") 'rest-utils--close-buffer))
 
+(defun rest-post--format-field (title-value)
+  (concat (first title-value) ":" (second title-value)))
+
+(defun rest-post--data-to-printable-lines (post-data)
+    (seq-map 'rest-post--format-field (list (list "title" (cdr (assoc 'title post-data)))
+                                            (list "body" (cdr (assoc 'body post-data))))))
+
 (defun rest-post--format-post (post-data)
-  (format "%s" post-data))
+  (seq-reduce (lambda (acc line)
+                (concat acc "\n" line))
+              (rest-post--data-to-printable-lines post-data)
+              ""))
 
 (defun rest-post--read-formatted-post (id)
   (rest-post--format-post (rest-api--read-post id)))
