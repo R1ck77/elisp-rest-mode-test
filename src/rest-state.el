74d3354aaa2d22a7b8681be2799a33ade1285820
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-02 01:30:51 +0100

    Macro based state management

--- src/rest-state.el
+++ src/rest-state.el
@@ -3,24 +3,27 @@
 (defvar rest-state--users nil)
 (defvar rest-state--posts nil)
 
+(defmacro rest-state--create-cached-getter (cache function)
+  (let ((cached (make-symbol "cached"))
+        (value (make-symbol "value")))    
+    `(lambda (id)
+       (let ((,cached (alist-get id ,cache)))
+         (or ,cached
+             (let ((,value (funcall ,function id)))
+               (setq ,cache (cons (cons id ,value) ,cache))
+               ,value))))))
+
+(defun rest-state-get-user-with-id (id))
+(defun rest-state-get-post-with-id (id))
+
 (defun rest-state-init ()
-  ;;; (nil. nil) necessary to make nconc work. assoc doesn't care
-  (setq rest-state--users (cons nil nil))
-  (setq rest-state--posts (cons nil nil)))
-
-(defun rest-state--get-cached (cache function id)
-  (let ((cached (assoc id cache)))
-    (or cached
-      (let ((value (funcall function id)))
-        (nconc cache (list (cons id value)))
-        value))))
-
-(defun rest-state-get-user-with-id (user-id)
-  (rest-state--get-cached rest-state--users
-                          'rest-api-read-user user-id))
-
-(defun rest-state-get-post-with-id (post-id)
-  (rest-state--get-cached rest-state--posts
-                          'rest-api-read-post post-id))
+  (setq rest-state--users nil)
+  (fset 'rest-state-get-user-with-id
+        (rest-state--create-cached-getter rest-state--users
+                                          'rest-api-read-user))
+  (setq rest-state--posts nil)
+  (fset 'rest-state-get-post-with-id 
+        (rest-state--create-cached-getter rest-state--posts
+                                          'rest-api-read-post)))
 
 (provide 'rest-state)
