5a4384431df50246133a3982209a4fbe97a8a79e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-04 01:49:06 +0100

    Add basic author properties printing

--- src/rest-author.el
+++ src/rest-author.el
@@ -1,5 +1,15 @@
+(require 'rest-state)
+
+(defconst rest-author--buffer-name "*Author*")
+
+(defun rest-author--print (author-data)
+  (let ((result ""))
+    (dolist (cell author-data)
+      (setq result (concat result (format "%s\n" cell))))
+    result))
 
 (defun rest-author-show-author (author-id)
-  (message "** I should show the page for author %d now ***" author-id))
+  (let ((author-data (rest-state-get-user-with-id author-id)))
+      (rest-detail-show rest-author--buffer-name (rest-author--print author-data))))
 
 (provide 'rest-author)
