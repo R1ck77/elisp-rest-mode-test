44561fbb943919856ca945c22445c7e411805431
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-05 09:06:51 +0100

    Extract the fields printing in an external functionality

--- src/rest-detail.el
+++ src/rest-detail.el
@@ -27,18 +27,31 @@
   (lambda (field-content)
     (rest-detail-format-body (cdr field-content))))
 
-(defun rest-detail--convert-field (field-content formatters)
-  (let ((formatter (alist-get (car field-content) formatters)))
-    (if formatter
-        (funcall formatter field-content)
-      "")))
+(defun rest-detail--convert-field (field-formatter-data)
+  (funcall (cadr field-formatter-data) (cons (car field-formatter-data)
+                                           (elt field-formatter-data 2))))
+
+(defun rest-detail--not-nilp (x)
+  (not (null x)))
+
+(defun rest-detail--get-field-for-formatter (field-formatter all-data)
+  "Accepts a cons '(field . formatter) and the assoc. list of data, returns (field formatter data-content)
+
+nil is returned if there is no data for the formatter"
+  (let* ((field (car field-formatter))
+         (field-content (alist-get field all-data)))
+    (and field-content (list field (cdr field-formatter) field-content))))
 
 (defun rest-detail-format-data (data formatters)
   (seq-reduce 'concat
-   (seq-map (lambda (field-content)
-              (rest-detail--convert-field field-content formatters))
-            data)
-   ""))
+              (seq-map
+               'rest-detail--convert-field 
+               (seq-filter
+                'rest-detail--not-nilp
+                (seq-map (lambda (formatter-data)
+                           (rest-detail--get-field-for-formatter formatter-data data))
+                         formatters)))
+              ""))
 
 (defun rest-detail-show (buffer-name post-content)
   (with-current-buffer (rest-utils-context-buffer-bottom buffer-name)
